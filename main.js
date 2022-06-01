2af2c788e5abf30e12b61fff1cb92ee06a01e856
Lucas Oliveira <104081952+lucasofaria@users.noreply.github.com>
2022-06-01 20:31:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -21,7 +21,11 @@ function getGitHubProfileInfos() {
   fetch(url)
     .then(response => response.json())
     .then(data => {
+      userName.textContent = data.name
       userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
     })
 }
 
